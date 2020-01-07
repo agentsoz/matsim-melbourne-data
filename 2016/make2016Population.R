2e59b71650d3e758aaf1a8c719d55b320c9fc6e4
Dhirendra Singh <dhi.singh@gmail.com>
2020-01-07 17:25:39 +1100

    Towards writing MATSim popn XML

--- 2016/make2016Population.R
+++ 2016/make2016Population.R
@@ -3,139 +3,248 @@
 #
 
 suppressWarnings(library(markovchain))
+suppressWarnings(library(XML))
 source('persons/samplePersons.R')
 source('locations/importData.R')
+
+# create a trip chain for a person
+generateTripChain<-function(mc, SA1) {
+  
+  # internal function to general the trip chain
+  # TODO: SA1 has no bearing yet on the trip chain produced
+  generateChain<-function(mc, SA1) {
+    v<-c("Home Morning",rmarkovchain(n=100,mc,t0="Home Morning")) # chain of requested length
+    idx<-match("Home Night", v); # find index of last activity 
+    v<-v[seq(1,idx)] # remove repeating last activities
+    return(v)
+  }
+  
+  # internal function to replace activity tags with location tags
+  replaceActivityWithLocationTags<-function (tc) {
+    # convert activity-based tags to location-based tags (from SA1_attributes.sqlite) being: 
+    # Home* -> home
+    # Work -> work
+    # Study -> education
+    # Shop -> commercial
+    # Personal -> commercial
+    # Social/Recreational -> commercial,park
+    # Pickup/Dropoff/Deliver -> work,education,commercial,park (but not home)
+    # Other -> work,education,commercial,park (but not home)
+    tc<-replace(tc, tc=="Home Morning", "home")
+    tc<-replace(tc, tc=="Home Daytime", "home")
+    tc<-replace(tc, tc=="Home Night", "home")
+    tc<-replace(tc, tc=="Work", "work")
+    tc<-replace(tc, tc=="Study", "education")
+    tc<-replace(tc, tc=="Shop", "commercial")
+    tc<-replace(tc, tc=="Personal", "commercial")
+    tc<-replace(tc, tc=="Social/Recreational", "commercial")
+    
+    # KISS: replace 'With Someone' with Other for now
+    tc<-replace(tc, tc=="With Someone", "Other")
+    # KISS: assuming Social/Recreational is equally likely to occur in commercial or park locations ; improve later on
+    tc<-as.vector(sapply(tc, function(x) replace(x, x=="Social/Recreational", sample(c("commercial","park"), 1))))
+    # KISS: assuming Pickup/Dropoff/Deliver is equally likely to occur in any location; improve later on
+    tc<-as.vector(sapply(tc, function(x) replace(x, x=="Pickup/Dropoff/Deliver", sample(c("work","education","commercial","park"), 1))))
+    # KISS: assuming Other is equally likely to occur in any location; improve later on; improve later on
+    tc<-as.vector(sapply(tc, function(x) replace(x, x=="Other", sample(c("work","education","commercial","park"), 1))))
+    return(tc)
+  }
+  
+  # generate a new trip chain
+  tc<-generateChain(mc, null)
+  # For any chain with 'With Someone' discard and start again as we do not care about generating secondary persons
+  while("With Someone" %in% tc) {
+    tc<-generateChain(mc, null)
+  }
+  # KISS: Discarding trip chains with 'Mode Change' for now; improve later on
+  while("Mode Change" %in% tc) {
+    tc<-generateChain(mc, null)
+  }
+  
+  # TODO: remove successive home activities
+  # ...
+  
+  # replace activity tags with location tags
+  tc<-replaceActivityWithLocationTags(tc)
+  return(tc)
+}
+
+# split trip into tours
+# order of extraction of tours is: home, then work, then others
+# tripChainToTours<-function(tc) {
+# }
+
+
+# Function to generate MATSim person XML
+generateMATSimPersonXML <- function(pid, p, tc) {
+
+  # internal function to create activities and legs from given trip chain
+  createMATSimActivitiesAndLegs <- function(person, tc) {
+    # get a person and determine its home SA1 and coordinates
+    home_sa1<-as.character(person$SA1_MAINCODE_2016)
+    home_xy<-getAddressCoordinates(home_sa1,"home")
+    
+    # data frames for storing this person's activities and connecting legs
+    acts<-data.frame(id=NA, type=NA, sa1=NA, x=NA, y=NA, start_hhmmss=NA, end_hhmmss=NA)
+    legs<-data.frame(origin_act_id=NA,mode=NA,dest_act_id=NA)
+    
+    # first activity is always home
+    if (tc[1] != "home") stop(paste0('First activity in trip chain must be `home` but was `',tc[1],'`'))
+    acts[1,]$id<-1
+    acts[1,]$type<-"home"
+    acts[1,]$sa1<-home_sa1
+    acts[1,]$x<-home_xy[1]
+    acts[1,]$y<-home_xy[2]
+    # determine the SA1 and coordinartes for the remaining activites
+    mode<-NULL
+    work_sa1<-NULL; work_xy<-NULL
+    for(i in 2:length(tc)) {
+      acts[i,]$id<-i
+      acts[i,]$type<-tc[i]
+      # determine SA1 for this activity type given last SA1
+      if (is.null(mode) || tc[i-1]=="home") { # mode can change if last activity was home
+        df<-findLocation(acts[i-1,]$sa1,acts[i,]$type)
+      } else {
+        df<-findLocationKnownMode(acts[i-1,]$sa1, acts[i,]$type, mode)
+      }
+      # assign the SA1 and coords
+      if(tc[i]=="home") { # re-use home SA1 and coords
+        acts[i,]$sa1<-home_sa1 
+        acts[i,]$x<-home_xy[1]
+        acts[i,]$y<-home_xy[2]
+      } else if(tc[i]=="work" && !is.null(work_sa1)) { # re-use work SA1 and coords
+        acts[i,]$sa1<-work_sa1
+        acts[i,]$x<-work_xy[1]
+        acts[i,]$y<-work_xy[2]
+      } else {
+        acts[i,]$sa1<-df[2]
+        xy<-getAddressCoordinates(acts[i,]$sa1,acts[i,]$type)
+        acts[i,]$x<-xy[1]
+        acts[i,]$y<-xy[2]
+        # if this is a work activity then also save its SA1 and XY coordinates for future re-use
+        if(tc[i]=="work" && is.null(work_sa1)) {
+          work_sa1<-df[2]
+          work_xy<-xy
+        }
+      }
+      # TODO: assign sensible start and end times for activities
+      
+      # save the leg
+      mode=df[1]
+      legs[i-1,]$origin_act_id<-acts[i-1,]$id
+      legs[i-1,]$dest_act_id<-acts[i,]$id
+      legs[i-1,]$mode<-mode
+    }
+    return(list(acts,legs))
+  }
+  
+  ### internal function to generate MATSim person attributes
+  attachMATSimPersonAttributes <- function(pp,p) {
+    attrs<-drop(as.matrix(p)) # get named vector of attributes
+    # create person attributes
+    xattr<-lapply(
+      seq_along(attrs),
+      function(i,x,n) { 
+        xx<-newXMLNode("attribute", attrs=c(name = n[[i]], class="java.lang.String"))
+        xmlValue(xx)<-x[[i]]
+        xx
+      }, 
+      x=attrs, 
+      n=names(attrs))
+    # assign attribute list to attributes tag
+    xattrs<-newXMLNode("attributes")
+    addChildren(xattrs,xattr) 
+    # attach attributes to person
+    addChildren(pp,xattrs) 
+    return(pp)
+  }
+  
+  ### internal function to generate MATSim person plan of activities and legs
+  attachMATSimPersonPlanXML<- function(pp, acts, legs) {
+    # create the activities
+    xacts<-apply(
+      acts, 1,
+      function(x) {
+        n<-newXMLNode("activity", attrs=c(type=x[[2]], x=x[[4]], y=x[[5]], end_time=x[[7]]))
+      })
+    # create the legs
+    xlegs<-apply(
+      legs, 1,
+      function(x) { 
+        n<-newXMLNode("leg", attrs=c(x[2]))
+        n
+      })
+    #interleave the activities and legs
+    idx <- order(c(seq_along(xacts), seq_along(xlegs)))
+    xactslegs<-(c(xacts,xlegs))[idx]
+    #create a new plan
+    xplan<-newXMLNode("plan", attrs=c(selected="yes"))
+    # attach the activities and legs to the plan
+    addChildren(xplan,xactslegs)
+    #attach plan to person
+    addChildren(pp,xplan)
+    return(pp)  
+  }
+  
+  # build activities and legs for the person
+  df<-createMATSimActivitiesAndLegs(p, tc)
+  acts<-df[[1]]
+  legs<-df[[2]]
+  # new XML node for this person
+  pp<-newXMLNode("person", attrs=c(id=pid))
+  # attach person attributes to XML node
+  pp<-attachMATSimPersonAttributes(pp,p) 
+  # attach plan with activities and legs to XML node
+  pp<-attachMATSimPersonPlanXML(pp, acts, legs)
+  # return the XML node
+  return(pp)
+}  
+
+# sample trip chain for testing
+#tc<-c("Home Morning", "Pickup/Dropoff/Deliver", "Home Daytime", "Social/Recreational",    
+#      "Social/Recreational", "Home Daytime", "Pickup/Dropoff/Deliver", "Pickup/Dropoff/Deliver", "Home Night")
+# tc<-c(
+#   "Home Morning",           "Shop",                   "Social/Recreational",    "Home Daytime",          
+#   "Pickup/Dropoff/Deliver", "Home Daytime",           "Shop",                   "Home Daytime",          
+#   "With Someone",           "Home Daytime",           "Pickup/Dropoff/Deliver", "Shop",                  
+#   "Home Daytime",           "Shop",                   "Work",                   "Home Night"  
+# )
+
+
+printProgress<-function(row) {
+  if((row-1)%%50==0) cat(paste0(as.character(Sys.time()),' |'))
+  if(row%%10==0) {
+    cat('|')
+  } else {
+    cat('.')
+  }
+  if(row%%50==0) cat(paste0(' ', row,'\n'))
+}
+
+### START HERE
+
+# Read the markov chain model for trip chains
+mc<-readRDS('./activities/vista_2012_16_extracted_activities_weekday_markov_chain_model.rds')
 
 # sample N persons from the population
 orig<-samplePersons(1000)
 persons<-assignSa1Maincode(orig)
 persons<-as.data.frame(persons)
-# Read the markov chain model for trip chains
-mc<-readRDS('./activities/vista_2012_16_extracted_activities_weekday_markov_chain_model.rds')
 
 
-# create a trip chain for a person
-generateTripChain<-function(mc, SA1) {
-  v<-c("Home Morning",rmarkovchain(n=100,mc,t0="Home Morning")) # chain of requested length
-  idx<-match("Home Night", v); # find index of last activity 
-  v<-v[seq(1,idx)] # remove repeating last activities
-  v
+# create MATSim population XML
+popn<-newXMLNode("population")
+for (row in 1:nrow(persons)) {
+  printProgress(row)
+  # get the person
+  p<-persons[row,]
+  # generate a trip chain for this person
+  tc<-generateTripChain(mc,p$SA1_MAINCODE_2016)
+  # generate MATSim XML for this person
+  pp<-generateMATSimPersonXML(row-1, p, tc)
+  # attach this person to the population
+  addChildren(popn,pp)
 }
-
-# split trip into tours
-# order of extraction of tours is: home, then work, then others
-tripChainToTours<-function(tc) {
-  
-}
-
-# Generate a new trip chain
-tc<-generateTripChain(mc, null)
-
-# For any chain with 'With Someone' discard and start again as we do not care about generating secondary persons
-while("With Someone" %in% tc) {
-  tc<-generateTripChain(mc, null)
-}
-# KISS: Discarding trip chains with 'Mode Change' for now; improve later on
-while("Mode Change" %in% tc) {
-  tc<-generateTripChain(mc, null)
-}
-
-# use this sample for now testing
-#tc<-c("Home Morning", "Pickup/Dropoff/Deliver", "Home Daytime", "Social/Recreational",    
-#      "Social/Recreational", "Home Daytime", "Pickup/Dropoff/Deliver", "Pickup/Dropoff/Deliver", "Home Night")
-tc<-c(
-  "Home Morning",           "Shop",                   "Social/Recreational",    "Home Daytime",          
-  "Pickup/Dropoff/Deliver", "Home Daytime",           "Shop",                   "Home Daytime",          
-  "With Someone",           "Home Daytime",           "Pickup/Dropoff/Deliver", "Shop",                  
-  "Home Daytime",           "Shop",                   "Work",                   "Home Night"  
-)
-
-# convert activity-based tags to location-based tags (from SA1_attributes.sqlite) being: 
-# Home* -> home
-# Work -> work
-# Study -> education
-# Shop -> commercial
-# Personal -> commercial
-# Social/Recreational -> commercial,park
-# Pickup/Dropoff/Deliver -> work,education,commercial,park (but not home)
-# Other -> work,education,commercial,park (but not home)
-tc<-replace(tc, tc=="Home Morning", "home")
-tc<-replace(tc, tc=="Home Daytime", "home")
-tc<-replace(tc, tc=="Home Night", "home")
-tc<-replace(tc, tc=="Work", "work")
-tc<-replace(tc, tc=="Study", "education")
-tc<-replace(tc, tc=="Shop", "commercial")
-tc<-replace(tc, tc=="Personal", "commercial")
-tc<-replace(tc, tc=="Social/Recreational", "commercial")
-
-# KISS: replace 'With Someone' with Other for now
-tc<-replace(tc, tc=="With Someone", "Other")
-# KISS: assuming Social/Recreational is equally likely to occur in commercial or park locations ; improve later on
-tc<-as.vector(sapply(tc, function(x) replace(x, x=="Social/Recreational", sample(c("commercial","park"), 1))))
-# KISS: assuming Pickup/Dropoff/Deliver is equally likely to occur in any location; improve later on
-tc<-as.vector(sapply(tc, function(x) replace(x, x=="Pickup/Dropoff/Deliver", sample(c("work","education","commercial","park"), 1))))
-# KISS: assuming Other is equally likely to occur in any location; improve later on; improve later on
-tc<-as.vector(sapply(tc, function(x) replace(x, x=="Other", sample(c("work","education","commercial","park"), 1))))
-
-
-# TODO: remove successive home activities
-# ...
-
-# get a person and determine its home SA1 and coordinates
-p<-persons[1,]
-home_sa1<-as.character(p$SA1_MAINCODE_2016)
-home_xy<-getAddressCoordinates(acts[1,]$sa1,"home")
-
-# data frames for storing this person's activities and connecting legs
-acts<-data.frame(id=NA, type=NA, sa1=NA, x=NA, y=NA, start_hhmm=NA, end_hhmm=NA)
-legs<-data.frame(origin_act_id=NA,mode=NA,dest_act_id=NA)
-
-# first activity is always home
-if (tc[1] != "home") stop(paste0('First activity in trip chain must be `home` but was `',tc[1],'`'))
-acts[1,]$id<-1
-acts[1,]$type<-"home"
-acts[1,]$sa1<-home_sa1
-acts[1,]$x<-home_xy[1]
-acts[1,]$y<-home_xy[2]
-# determine the SA1 and coordinartes for the remaining activites
-mode<-NULL
-work_sa1<-NULL; work_xy<-NULL
-for(i in 2:length(tc)) {
-  acts[i,]$id<-i
-  acts[i,]$type<-tc[i]
-  # determine SA1 for this activity type given last SA1
-  if (is.null(mode) || tc[i-1]=="home") { # mode can change if last activity was home
-    df<-findLocation(acts[i-1,]$sa1,acts[i,]$type)
-  } else {
-    df<-findLocationKnownMode(acts[i-1,]$sa1, acts[i,]$type, mode)
-  }
-  # if this is a work activity then also save its SA1 and XY coordinates for future re-use
-  if(tc[i]=="work" && is.null(work_sa1)) {
-    work_sa1<-df[2]
-    work_xy<-xy
-  }
-  # assign the SA1 and coords
-  if(tc[i]=="home") { # re-use home SA1 and coords
-    acts[i,]$sa1<-home_sa1 
-    acts[i,]$x<-home_xy[1]
-    acts[i,]$y<-home_xy[2]
-  } else if(tc[i]=="work") { # re-use work SA1 and coords
-    acts[i,]$sa1<-work_sa1
-    acts[i,]$x<-work_xy[1]
-    acts[i,]$y<-work_xy[2]
-  } else {
-    acts[i,]$sa1<-df[2]
-    xy<-getAddressCoordinates(acts[i,]$sa1,acts[i,]$type)
-    acts[i,]$x<-xy[1]
-    acts[i,]$y<-xy[2]
-  }
-  # save the leg
-  mode=df[1]
-  legs[i-1,]$origin_act_id<-acts[i-1,]$id
-  legs[i-1,]$dest_act_id<-acts[i,]$id
-  legs[i-1,]$mode<-mode
-}
-
 
 #tripChainToTours(tc)
 
