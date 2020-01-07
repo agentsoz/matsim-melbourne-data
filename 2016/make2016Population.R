d2ab18cd2c71522a13be58532cc2f086c6013de4
Dhirendra Singh <dhi.singh@gmail.com>
2020-01-07 15:17:52 +1100

    First cut at assigning locations/modes to activites

--- 2016/make2016Population.R
+++ 2016/make2016Population.R
@@ -1,5 +1,5 @@
 #
-# Be wary of the KISS (Keep It Simple Stupid) assumptions and simplifications below
+# Be wary of KISS (Keep It Simple Stupid) assumptions and simplifications below
 #
 
 suppressWarnings(library(markovchain))
@@ -7,9 +7,9 @@ source('persons/samplePersons.R')
 source('locations/importData.R')
 
 # sample N persons from the population
-#orig<-samplePersons(100)
-#persons<-assignSa1Maincode(orig)
-
+orig<-samplePersons(1000)
+persons<-assignSa1Maincode(orig)
+persons<-as.data.frame(persons)
 # Read the markov chain model for trip chains
 mc<-readRDS('./activities/vista_2012_16_extracted_activities_weekday_markov_chain_model.rds')
 
@@ -57,8 +57,8 @@ tc<-c(
 # Shop -> commercial
 # Personal -> commercial
 # Social/Recreational -> commercial,park
-# Pickup/Dropoff/Deliver -> home,work,education,commercial,park
-# Other -> home,work,education,commercial,park
+# Pickup/Dropoff/Deliver -> work,education,commercial,park (but not home)
+# Other -> work,education,commercial,park (but not home)
 tc<-replace(tc, tc=="Home Morning", "home")
 tc<-replace(tc, tc=="Home Daytime", "home")
 tc<-replace(tc, tc=="Home Night", "home")
@@ -67,14 +67,75 @@ tc<-replace(tc, tc=="Study", "education")
 tc<-replace(tc, tc=="Shop", "commercial")
 tc<-replace(tc, tc=="Personal", "commercial")
 tc<-replace(tc, tc=="Social/Recreational", "commercial")
+
+# KISS: replace 'With Someone' with Other for now
+tc<-replace(tc, tc=="With Someone", "Other")
 # KISS: assuming Social/Recreational is equally likely to occur in commercial or park locations ; improve later on
 tc<-as.vector(sapply(tc, function(x) replace(x, x=="Social/Recreational", sample(c("commercial","park"), 1))))
 # KISS: assuming Pickup/Dropoff/Deliver is equally likely to occur in any location; improve later on
-tc<-as.vector(sapply(tc, function(x) replace(x, x=="Pickup/Dropoff/Deliver", sample(c("home","work","education","commercial","park"), 1))))
+tc<-as.vector(sapply(tc, function(x) replace(x, x=="Pickup/Dropoff/Deliver", sample(c("work","education","commercial","park"), 1))))
 # KISS: assuming Other is equally likely to occur in any location; improve later on; improve later on
-tc<-as.vector(sapply(tc, function(x) replace(x, x=="Other", sample(c("home","work","education","commercial","park"), 1))))
+tc<-as.vector(sapply(tc, function(x) replace(x, x=="Other", sample(c("work","education","commercial","park"), 1))))
 
 
+# TODO: remove successive home activities
+# ...
+
+# get a person and determine its home SA1 and coordinates
+p<-persons[1,]
+home_sa1<-as.character(p$SA1_MAINCODE_2016)
+home_xy<-getAddressCoordinates(acts[1,]$sa1,"home")
+
+# data frames for storing this person's activities and connecting legs
+acts<-data.frame(id=NA, type=NA, sa1=NA, x=NA, y=NA, start_hhmm=NA, end_hhmm=NA)
+legs<-data.frame(origin_act_id=NA,mode=NA,dest_act_id=NA)
+
+# first activity is always home
+if (tc[1] != "home") stop(paste0('First activity in trip chain must be `home` but was `',tc[1],'`'))
+acts[1,]$id<-1
+acts[1,]$type<-"home"
+acts[1,]$sa1<-home_sa1
+acts[1,]$x<-home_xy[1]
+acts[1,]$y<-home_xy[2]
+# determine the SA1 and coordinartes for the remaining activites
+mode<-NULL
+work_sa1<-NULL; work_xy<-NULL
+for(i in 2:length(tc)) {
+  acts[i,]$id<-i
+  acts[i,]$type<-tc[i]
+  # determine SA1 for this activity type given last SA1
+  if (is.null(mode) || tc[i-1]=="home") { # mode can change if last activity was home
+    df<-findLocation(acts[i-1,]$sa1,acts[i,]$type)
+    cat(paste0('new mode from act ',acts[i-1,]$id,'->',acts[i,]$id, ' is ', df[1], '\n'))
+  } else {
+    df<-findLocationKnownMode(acts[i-1,]$sa1, acts[i,]$type, mode)
+  }
+  # if this is a work activity then also save its SA1 and XY coordinates for future re-use
+  if(tc[i]=="work" && is.null(work_sa1)) {
+    work_sa1<-df[2]
+    work_xy<-xy
+  }
+  # assign the SA1 and coords
+  if(tc[i]=="home") { # re-use home SA1 and coords
+    acts[i,]$sa1<-home_sa1 
+    acts[i,]$x<-home_xy[1]
+    acts[i,]$y<-home_xy[2]
+  } else if(tc[i]=="work") { # re-use work SA1 and coords
+    acts[i,]$sa1<-work_sa1
+    acts[i,]$x<-work_xy[1]
+    acts[i,]$y<-work_xy[2]
+  } else {
+    acts[i,]$sa1<-df[2]
+    xy<-getAddressCoordinates(acts[i,]$sa1,acts[i,]$type)
+    acts[i,]$x<-xy[1]
+    acts[i,]$y<-xy[2]
+  }
+  # save the leg
+  mode=df[1]
+  legs[i-1,]$origin_act_id<-acts[i-1,]$id
+  legs[i-1,]$dest_act_id<-acts[i,]$id
+  legs[i-1,]$mode<-mode
+}
 
 
 #tripChainToTours(tc)
